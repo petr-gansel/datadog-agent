77aa77aa371192588a94c74083d95d65ebfa79d0
usamasaqib <usamasaqib.96@live.com>
2023-09-22 14:46:52 +0200

    use in-ram ro shared mount to read dependencies

--- test/new-e2e/system-probe/test-runner/main.go
+++ test/new-e2e/system-probe/test-runner/main.go
@@ -38,7 +38,7 @@ type TestConfig struct {
 
 const (
 	Testsuite   = "testsuite"
-	TestDirRoot = "/opt/system-probe-tests"
+	TestDirRoot = "/opt/kernel-version-testing/opt/system-probe-tests"
 	GoTestSum   = "/go/bin/gotestsum"
 
 	XMLDir       = "junit"
